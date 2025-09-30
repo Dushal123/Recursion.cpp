001ff37fcf22712fdd81ce73283a2c56cece9a8a
Dushal123 <kelladushal501@gmail.com>
2025-09-30 16:33:46 +0530

    Reverse integer.cpp

---
+++ Reverse integer.cpp
@@ -0,0 +1,26 @@
+/*
+Name:- Dushal kella
+PRN:- 24070123053
+Batch:- A2
+*/
+#include <iostream>
+using namespace std;
+
+void print_rev(int i){
+    if(i>0){
+        cout << (i%10);
+        print_rev(i/10);
+    }
+}
+
+int main(){
+    int n;
+    cout << "Type number you want to reverse: ";
+    cin >> n;
+    print_rev(n);
+}
+/*
+Output:-
+Type number you want to reverse: 1050302
+2030501
+*/
