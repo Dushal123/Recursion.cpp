c6e5e173ce02df9b5f7d508e8f4ff6cf07185f08
Dushal123 <kelladushal501@gmail.com>
2025-09-30 16:32:29 +0530

    print string.cpp

---
+++ print string.cpp
@@ -0,0 +1,24 @@
+/*
+Name:- Dushal kella
+PRN:- 24070123053
+Batch:- A2
+*/
+#include <iostream>
+using namespace std;
+
+void print_str(char *str){
+    if(*str !='\0'){
+        print_str(str+1);
+        cout << *str;
+    }
+}
+ 
+int main(){
+    string s="Rachit";
+    cout << "Reversed String: ";
+    print_str(&s[0]);
+}
+/*
+Output-
+Reversed String: tihcaR
+*/
